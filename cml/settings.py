bc296f6bc5f400a65d2725678c4714b46f5811c8
mel <devnull@localhost>
2012-07-23 14:39:21 -0400

    first commit

--- cml/settings.py
+++ cml/settings.py
@@ -152,23 +152,6 @@ STATICFILES_FINDERS = (
 # DATABASES #
 #############
 
-DATABASES = {
-    "default": {
-        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        # DB name or path to database file if using sqlite3.
-        "NAME": "criticalmediaproject",
-        # Not used with sqlite3.
-        "USER": "cml",
-        # Not used with sqlite3.
-        "PASSWORD": "o4zeQc7T",
-        # Set to empty string for localhost. Not used with sqlite3.
-        "HOST": "",
-        # Set to empty string for default. Not used with sqlite3.
-        "PORT": "",
-    }
-}
-
 
 #########
 # PATHS #
