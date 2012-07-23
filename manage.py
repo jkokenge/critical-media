00a1247a849154c4c9068c1f9202c797725c7ec8
mel <devnull@localhost>
2012-07-23 16:39:00 -0400

    settings fix

--- manage.py
+++ manage.py
@@ -32,5 +32,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cml.settings")
 
     from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
