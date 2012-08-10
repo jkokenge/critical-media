a4ba622c95a7c76558f8f8dda252592b1dcbf2f7
mloudon <devnull@localhost>
2012-08-10 09:41:12 -0400

    two-column list

--- cmlproject/templatetags/cml_tags.py
+++ cmlproject/templatetags/cml_tags.py
@@ -33,3 +33,34 @@ def add_glossary_tooltips(text):
             words[i] = '<a href="%s" rel="tooltip" title="%s">%s</a>' % (terms[words[i].upper()][1],terms[words[i].upper()][0], words[i])
                                                                                 
     return "".join(words)
+
+@register.filter
+def partition(thelist, n):
+    """
+    http://djangosnippets.org/snippets/6/
+    
+    Break a list into ``n`` pieces. The last list may be larger than the rest if
+    the list doesn't break cleanly. That is::
+
+        >>> l = range(10)
+
+        >>> partition(l, 2)
+        [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+
+        >>> partition(l, 3)
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8, 9]]
+
+        >>> partition(l, 4)
+        [[0, 1], [2, 3], [4, 5], [6, 7, 8, 9]]
+
+        >>> partition(l, 5)
+        [[0, 1], [2, 3], [4, 5], [6, 7], [8, 9]]
+
+    """
+    try:
+        n = int(n)
+        thelist = list(thelist)
+    except (ValueError, TypeError):
+        return [thelist]
+    p = len(thelist) / n
+    return [thelist[p*i:p*(i+1)] for i in range(n - 1)] + [thelist[p*(i+1):]]
