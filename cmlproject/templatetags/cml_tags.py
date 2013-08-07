b6277e640a4078e7ecec66f8f3d9fbdbd8bbd4da
mloudon <devnull@localhost>
2013-08-06 22:23:53 -0400

    stop tooltip from applying to href

--- cmlproject/templatetags/cml_tags.py
+++ cmlproject/templatetags/cml_tags.py
@@ -26,13 +26,18 @@ def active(request, url_name, *myargs):
 @register.filter
 def add_glossary_tooltips(text):
     terms = dict((t.name.upper(), [t.explanation, t.get_absolute_url()]) for t in GlossaryTerm.objects.all())
-    words = re.split('(\W)',text)
-        
-    for i in range(len(words)):
-        if (not words[i]==" ") and not (words[i].startswith("href=")) and (words[i].upper() in terms):
-            words[i] = '<a href="%s" rel="tooltip" title="%s">%s</a>' % (terms[words[i].upper()][1],terms[words[i].upper()][0], words[i])
-                                                                                
-    return "".join(words)
+    tipped_text = ""    
+    for chunk in re.split('(<.*?>',text):
+        if not chunk.startswith("<"):
+            words = re.split('(\W)',text)
+                
+            for i in range(len(words)):
+                if (not words[i]==" ") and (words[i].upper() in terms):
+                    words[i] = '<a href="%s" rel="tooltip" title="%s">%s</a>' % (terms[words[i].upper()][1],terms[words[i].upper()][0], words[i])
+                                                                                        
+            chunk = "".join(words)
+        tipped_text.join(chunk)
+    return tipped_text
 
 @register.filter
 def add_title_classes(text):
