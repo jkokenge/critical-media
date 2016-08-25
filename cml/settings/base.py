42090f1e8b7e309f74482a35ec07e5749260f3ec
Joseph Kokenge <jkokenge@gmail.com>
2016-08-24 21:02:07 -0500

    split apart the settings files even more

--- cml/settings/base.py
+++ cml/settings/base.py
@@ -111,57 +111,57 @@ STATICFILES_FINDERS = (
 )
 
 
-#########
-# PATHS #
-#########
+# #########
+# # PATHS #
+# #########
 
-# Full filesystem path to the project.
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print("project_root %s") % PROJECT_ROOT
+# # Full filesystem path to the project.
+# # PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# # print("project_root %s") % PROJECT_ROOT
 
-# Name of the directory for the project.
-PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+# # # Name of the directory for the project.
+# # PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
 
-print("project_dirname %s") % PROJECT_DIRNAME
+# # print("project_dirname %s") % PROJECT_DIRNAME
 
-# Every cache key will get prefixed with this value - here we set it to
-# the name of the directory the project is in to try and use something
-# project specific.
-CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
+# # Every cache key will get prefixed with this value - here we set it to
+# # the name of the directory the project is in to try and use something
+# # project specific.
+# CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
 
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/static/"
+# # URL prefix for static files.
+# # Example: "http://media.lawrence.com/static/"
+# STATIC_URL = "/static/"
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+# # Absolute path to the directory static files should be collected to.
+# # Don't put anything in this directory yourself; store your static files
+# # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# # Example: "/home/media/media.lawrence.com/static/"
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = STATIC_URL + "media/"
+# # URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# # trailing slash.
+# # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+# MEDIA_URL = STATIC_URL + "media/"
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+# # Absolute filesystem path to the directory that will hold user-uploaded files.
+# # Example: "/home/media/media.lawrence.com/media/"
+# MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+# # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# # trailing slash.
+# # Examples: "http://foo.com/media/", "/media/".
+# ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
 
-# Package/module name to import the root urlpatterns from for the project.
-#ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
-#print("ROOT_URLCONF %s") % ROOT_URLCONF
+# # Package/module name to import the root urlpatterns from for the project.
+# #ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+# #print("ROOT_URLCONF %s") % ROOT_URLCONF
 
-# Put strings here, like "/home/html/django_templates"
-# or "C:/www/django/templates".
-# Always use forward slashes, even on Windows.
-# Don't forget to use absolute paths, not relative paths.
-TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
+# # Put strings here, like "/home/html/django_templates"
+# # or "C:/www/django/templates".
+# # Always use forward slashes, even on Windows.
+# # Don't forget to use absolute paths, not relative paths.
+# TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
 
 
 ################
--- cml/settings/prod.py
+++ cml/settings/prod.py
@@ -1,6 +1,59 @@
 from .base import *
 from os import environ
 
+# Full filesystem path to the project.
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print("project_root %s") % PROJECT_ROOT
+
+# # Name of the directory for the project.
+PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+
+print("project_dirname %s") % PROJECT_DIRNAME
+
+# Every cache key will get prefixed with this value - here we set it to
+# the name of the directory the project is in to try and use something
+# project specific.
+CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_DIRNAME
+
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
+
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = STATIC_URL + "media/"
+print("media url: %s") % MEDIA_URL
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+
+print("media root: %s") % MEDIA_ROOT
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = STATIC_URL + "grappelli/"
+
+# Package/module name to import the root urlpatterns from for the project.
+#ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+#print("ROOT_URLCONF %s") % ROOT_URLCONF
+
+# Put strings here, like "/home/html/django_templates"
+# or "C:/www/django/templates".
+# Always use forward slashes, even on Windows.
+# Don't forget to use absolute paths, not relative paths.
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, "templates"),)
+
+ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
+###########################################################
+
 ROOT_URLCONF = "cmp.cml.urls"
 
 DEBUG = False
